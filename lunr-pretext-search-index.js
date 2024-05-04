7050ffb8538b49faa133f42819cbe893cfda6e80
Oscar Levin <oscar.levin@unco.edu>
2024-05-04 15:52:42 -0600

    Latest build deployed.

---
+++ lunr-pretext-search-index.js
@@ -0,0 +1,365 @@
+var ptx_lunr_search_style = "textbook";
+var ptx_lunr_docs = [
+{
+  "id": "frontmatter-2",
+  "level": "1",
+  "url": "frontmatter-2.html",
+  "type": "Colophon",
+  "number": "",
+  "title": "Colophon",
+  "body": "  My Website   copyright  "
+},
+{
+  "id": "frontmatter-3",
+  "level": "1",
+  "url": "frontmatter-3.html",
+  "type": "Dedication",
+  "number": "",
+  "title": "Dedication",
+  "body": " For ...  "
+},
+{
+  "id": "frontmatter-4",
+  "level": "1",
+  "url": "frontmatter-4.html",
+  "type": "Acknowledgements",
+  "number": "",
+  "title": "Acknowledgements",
+  "body": " I would like to thank...  "
+},
+{
+  "id": "frontmatter-preface",
+  "level": "1",
+  "url": "frontmatter-preface.html",
+  "type": "Preface",
+  "number": "",
+  "title": "Preface",
+  "body": "   About this book:  "
+},
+{
+  "id": "frontmatter-6",
+  "level": "1",
+  "url": "frontmatter-6.html",
+  "type": "Preface",
+  "number": "",
+  "title": "Not just that..",
+  "body": " Not just that..  I still have more to say!  "
+},
+{
+  "id": "sec-first-intro",
+  "level": "1",
+  "url": "sec-first-intro.html",
+  "type": "Section",
+  "number": "1.1",
+  "title": "Introduction to First Chapter",
+  "body": " Introduction to First Chapter   Intro to section, because we will have subsections.  Another paragraph of the introduction.    A subsection  Content of the subsection.  More content.    Another Subsection  Text with some math: only for some values of .    After the last subsection, you might have a conclusion.   "
+},
+{
+  "id": "sec-first-examples",
+  "level": "1",
+  "url": "sec-first-examples.html",
+  "type": "Section",
+  "number": "1.2",
+  "title": "Examples",
+  "body": " Examples  Here is an example:    The statement of the example, since it will have a solution. What is the solution?    This is the solution.    Here is another example, but without a solution   This is an example of an example without a solution (or hint or answer) so the paragraph doesn't need to be inside a statement tag.   Although the next things are not examples, they give examples of blocks that are not examples. First a theorem.   Theorem Title   This is the statement of the theorem.    And this is its proof.      A corollary that doesn't need a proof.    Perhaps at the end of the section, you want to add a note to the reader about how what you have fits into the larger scope of mathematics.   Larger Context  Comment to the reader here.   Of course, if you want more information, you might see .  "
+},
+{
+  "id": "sec-first-examples-3",
+  "level": "2",
+  "url": "sec-first-examples.html#sec-first-examples-3",
+  "type": "Example",
+  "number": "1.2.1",
+  "title": "",
+  "body": "  The statement of the example, since it will have a solution. What is the solution?    This is the solution.   "
+},
+{
+  "id": "sec-first-examples-5",
+  "level": "2",
+  "url": "sec-first-examples.html#sec-first-examples-5",
+  "type": "Example",
+  "number": "1.2.2",
+  "title": "",
+  "body": " This is an example of an example without a solution (or hint or answer) so the paragraph doesn't need to be inside a statement tag.  "
+},
+{
+  "id": "thm-first-examples-theorem",
+  "level": "2",
+  "url": "sec-first-examples.html#thm-first-examples-theorem",
+  "type": "Theorem",
+  "number": "1.2.3",
+  "title": "Theorem Title.",
+  "body": " Theorem Title   This is the statement of the theorem.    And this is its proof.   "
+},
+{
+  "id": "cor-first-examples-corollary",
+  "level": "2",
+  "url": "sec-first-examples.html#cor-first-examples-corollary",
+  "type": "Corollary",
+  "number": "1.2.4",
+  "title": "",
+  "body": "  A corollary that doesn't need a proof.   "
+},
+{
+  "id": "remark-first-examples-context",
+  "level": "2",
+  "url": "sec-first-examples.html#remark-first-examples-context",
+  "type": "Remark",
+  "number": "1.2.5",
+  "title": "Larger Context.",
+  "body": " Larger Context  Comment to the reader here.  "
+},
+{
+  "id": "ex-first",
+  "level": "1",
+  "url": "ex-first.html",
+  "type": "Exercises",
+  "number": "1.3",
+  "title": "My Exercises",
+  "body": "  My Exercises   Is this an exercise?   Yes.   It is, but not a very good one.    Another, with a hint.   The hint.      "
+},
+{
+  "id": "ex-first-2",
+  "level": "2",
+  "url": "ex-first.html#ex-first-2",
+  "type": "Exercise",
+  "number": "1.3.1",
+  "title": "",
+  "body": " Is this an exercise?   Yes.   It is, but not a very good one.  "
+},
+{
+  "id": "ex-first-3",
+  "level": "2",
+  "url": "ex-first.html#ex-first-3",
+  "type": "Exercise",
+  "number": "1.3.2",
+  "title": "",
+  "body": " Another, with a hint.   The hint.  "
+},
+{
+  "id": "ch-empty",
+  "level": "1",
+  "url": "ch-empty.html",
+  "type": "Chapter",
+  "number": "2",
+  "title": "(No title)",
+  "body": " (No title)  Try adding your own content here! This chapter doesn't have sections.  "
+},
+{
+  "id": "sec-features-blocks",
+  "level": "1",
+  "url": "sec-features-blocks.html",
+  "type": "Section",
+  "number": "3.1",
+  "title": "Environments and Blocks",
+  "body": " Environments and Blocks  Some text   My Theorem   Theorem statement.    Proof of theorem.      Statement of example    The solution.    Now a figure.   A frog   A nice-looking frog with a longish description.    "
+},
+{
+  "id": "thm_features-blocks-my-theorem",
+  "level": "2",
+  "url": "sec-features-blocks.html#thm_features-blocks-my-theorem",
+  "type": "Theorem",
+  "number": "3.1.1",
+  "title": "My Theorem.",
+  "body": " My Theorem   Theorem statement.    Proof of theorem.   "
+},
+{
+  "id": "sec-features-blocks-4",
+  "level": "2",
+  "url": "sec-features-blocks.html#sec-features-blocks-4",
+  "type": "Example",
+  "number": "3.1.2",
+  "title": "",
+  "body": "  Statement of example    The solution.   "
+},
+{
+  "id": "sec-features-blocks-6",
+  "level": "2",
+  "url": "sec-features-blocks.html#sec-features-blocks-6",
+  "type": "Figure",
+  "number": "3.1.3",
+  "title": "",
+  "body": " A frog   A nice-looking frog with a longish description.   "
+},
+{
+  "id": "sec-latex-image",
+  "level": "1",
+  "url": "sec-latex-image.html",
+  "type": "Section",
+  "number": "4.1",
+  "title": "LaTeX-Image Example",
+  "body": " LaTeX-Image Example   The graph made by TikZ   A 5-cycle     "
+},
+{
+  "id": "fig-tikz",
+  "level": "2",
+  "url": "sec-latex-image.html#fig-tikz",
+  "type": "Figure",
+  "number": "4.1.1",
+  "title": "",
+  "body": " The graph made by TikZ   A 5-cycle    "
+},
+{
+  "id": "sec-sageplot",
+  "level": "1",
+  "url": "sec-sageplot.html",
+  "type": "Section",
+  "number": "4.2",
+  "title": "SagePlot Examples",
+  "body": " SagePlot Examples   The parabola made by SageMath   A parabola      The paraboloid made by SageMath   A paraboloid     "
+},
+{
+  "id": "fig-sage2d",
+  "level": "2",
+  "url": "sec-sageplot.html#fig-sage2d",
+  "type": "Figure",
+  "number": "4.2.1",
+  "title": "",
+  "body": " The parabola made by SageMath   A parabola    "
+},
+{
+  "id": "fig-sage3d",
+  "level": "2",
+  "url": "sec-sageplot.html#fig-sage3d",
+  "type": "Figure",
+  "number": "4.2.2",
+  "title": "",
+  "body": " The paraboloid made by SageMath   A paraboloid    "
+},
+{
+  "id": "sec-asymptote",
+  "level": "1",
+  "url": "sec-asymptote.html",
+  "type": "Section",
+  "number": "4.3",
+  "title": "Asymptote Example",
+  "body": " Asymptote Example   Canada   Image of a Canadian flag     "
+},
+{
+  "id": "fig-asymptote",
+  "level": "2",
+  "url": "sec-asymptote.html#fig-asymptote",
+  "type": "Figure",
+  "number": "4.3.1",
+  "title": "",
+  "body": " Canada   Image of a Canadian flag    "
+},
+{
+  "id": "sec-webwork",
+  "level": "1",
+  "url": "sec-webwork.html",
+  "type": "Section",
+  "number": "4.4",
+  "title": "WeBWorK Example",
+  "body": " WeBWorK Example    Compute the sum of and     Add and together.              "
+},
+{
+  "id": "sec-webwork-2",
+  "level": "2",
+  "url": "sec-webwork.html#sec-webwork-2",
+  "type": "Checkpoint",
+  "number": "4.4.1",
+  "title": "",
+  "body": "  Compute the sum of and     Add and together.             "
+},
+{
+  "id": "sec-youtube",
+  "level": "1",
+  "url": "sec-youtube.html",
+  "type": "Section",
+  "number": "4.5",
+  "title": "YouTube Example",
+  "body": " YouTube Example   "
+},
+{
+  "id": "sec-interactive",
+  "level": "1",
+  "url": "sec-interactive.html",
+  "type": "Section",
+  "number": "4.6",
+  "title": "Interactive Example",
+  "body": " Interactive Example    Infinity, from the JSXGraph Showcase    "
+},
+{
+  "id": "sec-interactive-2",
+  "level": "2",
+  "url": "sec-interactive.html#sec-interactive-2",
+  "type": "Figure",
+  "number": "4.6.1",
+  "title": "",
+  "body": " Infinity, from the JSXGraph Showcase   "
+},
+{
+  "id": "sec-codelens",
+  "level": "1",
+  "url": "sec-codelens.html",
+  "type": "Section",
+  "number": "4.7",
+  "title": "Codelens Example",
+  "body": " Codelens Example   Finding primes   def SieveOfEratosthenes(n): # array of type boolean with True values in it prime = [True for i in range(n + 1)] p = 2 while (p * p <= n): # If it remain unchanged it is prime if (prime[p] == True): # updating all the multiples for i in range(p * 2, n + 1, p): prime[i] = False p += 1 prime[0]= False prime[1]= False # Print for p in range(n + 1): if prime[p]: print (p,end=\" \") # main if __name__=='__main__': n = 20 print (\"The prime numbers smaller than or equal to\", n,\"is\") SieveOfEratosthenes(n)    "
+},
+{
+  "id": "sieve-python",
+  "level": "2",
+  "url": "sec-codelens.html#sieve-python",
+  "type": "Listing",
+  "number": "4.7.1",
+  "title": "",
+  "body": " Finding primes   def SieveOfEratosthenes(n): # array of type boolean with True values in it prime = [True for i in range(n + 1)] p = 2 while (p * p <= n): # If it remain unchanged it is prime if (prime[p] == True): # updating all the multiples for i in range(p * 2, n + 1, p): prime[i] = False p += 1 prime[0]= False prime[1]= False # Print for p in range(n + 1): if prime[p]: print (p,end=\" \") # main if __name__=='__main__': n = 20 print (\"The prime numbers smaller than or equal to\", n,\"is\") SieveOfEratosthenes(n)   "
+},
+{
+  "id": "backmatter-2",
+  "level": "1",
+  "url": "backmatter-2.html",
+  "type": "Appendix",
+  "number": "A",
+  "title": "Selected Hints",
+  "body": " Selected Hints  "
+},
+{
+  "id": "backmatter-3",
+  "level": "1",
+  "url": "backmatter-3.html",
+  "type": "Appendix",
+  "number": "B",
+  "title": "Selected Solutions",
+  "body": " Selected Solutions  "
+},
+{
+  "id": "backmatter-4",
+  "level": "1",
+  "url": "backmatter-4.html",
+  "type": "Appendix",
+  "number": "C",
+  "title": "List of Symbols",
+  "body": " List of Symbols   "
+},
+{
+  "id": "backmatter-5",
+  "level": "1",
+  "url": "backmatter-5.html",
+  "type": "Index",
+  "number": "",
+  "title": "Index",
+  "body": " Index   "
+},
+{
+  "id": "backmatter-6",
+  "level": "1",
+  "url": "backmatter-6.html",
+  "type": "Colophon",
+  "number": "",
+  "title": "Colophon",
+  "body": " This book was authored in PreTeXt .  "
+}
+]
+
+var ptx_lunr_idx = lunr(function () {
+  this.ref('id')
+  this.field('title')
+  this.field('body')
+  this.metadataWhitelist = ['position']
+
+  ptx_lunr_docs.forEach(function (doc) {
+    this.add(doc)
+  }, this)
+})
